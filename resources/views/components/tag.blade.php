e6453f669edeef34173a635a7a4412721802e28f
tandelnirav210 <tandelnirav210@gmail.com>
2025-11-16 14:21:05 +0530

    Added Migration, Models, Controller and Seeder. Also added PEST Testing

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,4 +1,4 @@
-@props(['size'=>'base'])
+@props(['size'=>'base', 'tag'])
 
 @php
 $classes = 'bg-white/10 hover:bg-white/25 font-bold rounded-xl transition-colors duration-300';
@@ -10,4 +10,4 @@ $classes = 'bg-white/10 hover:bg-white/25 font-bold rounded-xl transition-colors
     }
 @endphp
 
-<a href="#" class="{{ $classes  }}">{{$slot}}</a>
+<a href="/tags/{{strtolower($tag->name)}}" class="{{ $classes  }}">{{$tag->name}}</a>
